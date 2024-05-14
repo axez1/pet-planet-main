595d77a3c22b78dfef9261ee6795bd5eb8cb1a99
axez1 <axez1@mail.ru>
2024-05-14 15:03:41 +0300

    create market page

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const buttons = document.querySelectorAll('.store__category-button');
+
+const changeActiveBtn = (event) => {
+  event.preventDefault();
+  const target = event.target;
+
+  buttons.forEach((button) => {
+    button.classList.remove('store__category-button_active')
+  })
+
+  target.classList.add('store__category-button_active');
+}
+
+buttons.forEach((button) => {
+  button.addEventListener('click', changeActiveBtn)
+})
