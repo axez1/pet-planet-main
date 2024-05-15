ba59b4d972c1c75b42ef8ccc81ab239421746802
axez1 <axez1@mail.ru>
2024-05-15 23:26:16 +0300

    start render product to API

--- src/index.js
+++ src/index.js
@@ -14,3 +14,56 @@ const changeActiveBtn = (event) => {
 buttons.forEach((button) => {
   button.addEventListener('click', changeActiveBtn)
 })
+
+const API_URL = 'https://thoracic-quaint-daffodil.glitch.me';
+
+const productList = document.querySelector('.store__list');
+
+const createProductCard = (product) => {
+  const productCard = document.createElement('li');
+  productCard.classList.add('store__item');
+  productCard.innerHTML = `
+      <article class="store__product product">
+
+          <img src="${API_URL}${product.photoUrl}" alt="${product.name}" class="product__image" width="388" height="261">
+
+          <h3 class="product__title">${product.name}</h3>
+
+          <p class="product__price">${product.price}&nbsp;₽</p>
+
+          <button type="button" class="product_btn-add-cart">Заказать</button>
+
+      </article>
+  `;
+
+  return productCard;
+}
+
+const renderProducts = (products) => {
+  
+  productList.textContent = '';
+
+  products.forEach((product) => {
+    const productCard = createProductCard(product);
+
+    productList.append(productCard);
+  })
+}
+
+const fetchProductByCategory = async (category) => {
+  try {
+    const response = await fetch(`${API_URL}/api/products/category/${category}`);
+    if(!response.ok) {
+      throw new Error(response.status);
+    }
+
+    const products = await response.json();
+    console.log(products)
+
+    renderProducts(products);
+  } catch(error) {
+    console.error(`Ошибка запроса товара: ${error}`);
+  } 
+}
+
+fetchProductByCategory('Домики');
