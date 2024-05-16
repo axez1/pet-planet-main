d9cc88d739a4fa9c0da5a792ffc15f2bdcb5fbbe
axez1 <axez1@mail.ru>
2024-05-17 00:30:19 +0300

    work fetch category to API and push items to cart

--- src/index.js
+++ src/index.js
@@ -1,35 +1,26 @@
-const buttons = document.querySelectorAll('.store__category-button');
-
-const changeActiveBtn = (event) => {
-  event.preventDefault();
-  const target = event.target;
-
-  buttons.forEach((button) => {
-    button.classList.remove('store__category-button_active')
-  })
-
-  target.classList.add('store__category-button_active');
-}
-
-buttons.forEach((button) => {
-  button.addEventListener('click', changeActiveBtn)
-})
-
 const API_URL = 'https://thoracic-quaint-daffodil.glitch.me';
 
+const buttons = document.querySelectorAll('.store__category-button');
 const productList = document.querySelector('.store__list');
+const cartButton = document.querySelector('.store__cart-button');
+const modalOverlay = document.querySelector('.modal-overlay');
+const cartItemsList = document.querySelector('.modal__cart-items');
+const modalCloseButton = document.querySelector('.modal-overlay_close-button');
+const cartCount = cartButton.querySelector('.store__cart-cnt');
 
-const createProductCard = (product) => {
+
+
+const createProductCard = ({ photoUrl, name, price }) => {
   const productCard = document.createElement('li');
   productCard.classList.add('store__item');
   productCard.innerHTML = `
       <article class="store__product product">
 
-          <img src="${API_URL}${product.photoUrl}" alt="${product.name}" class="product__image" width="388" height="261">
+          <img src="${API_URL}${photoUrl}" alt="${name}" class="product__image" width="388" height="261">
 
-          <h3 class="product__title">${product.name}</h3>
+          <h3 class="product__title">${name}</h3>
 
-          <p class="product__price">${product.price}&nbsp;₽</p>
+          <p class="product__price">${price}&nbsp;₽</p>
 
           <button type="button" class="product_btn-add-cart">Заказать</button>
 
@@ -58,7 +49,6 @@ const fetchProductByCategory = async (category) => {
     }
 
     const products = await response.json();
-    console.log(products)
 
     renderProducts(products);
   } catch(error) {
@@ -66,4 +56,63 @@ const fetchProductByCategory = async (category) => {
   } 
 }
 
-fetchProductByCategory('Домики');
+const changeCategory = ({ target }) => {
+  const category = target.textContent;
+
+  buttons.forEach((button) => {
+    button.classList.remove('store__category-button_active')
+  })
+
+  target.classList.add('store__category-button_active');
+  fetchProductByCategory(category);
+}
+
+buttons.forEach((button) => {
+  button.addEventListener('click', changeCategory)
+  if(button.classList.contains('store__category-button_active')) {
+    fetchProductByCategory(button.textContent);
+  }
+})
+
+const renderCartItems = () => {
+  cartItemsList.textContent = '';
+  const cartItems = JSON.parse(localStorage.getItem('cartItems') || '[]');
+  cartItems.forEach(item => {
+    const listItem = document.createElement('li');
+    listItem.textContent = item;
+    cartItemsList.append(listItem);
+  })
+}
+
+cartButton.addEventListener('click', () => {
+  modalOverlay.style.display = 'flex';
+  renderCartItems();
+})
+
+modalOverlay.addEventListener('click', ({ target }) => {
+  if(target === modalOverlay || target.closest('.modal-overlay_close-button')) {
+    modalOverlay.style.display = 'none';
+  }
+})
+
+const updateCartCount = () => {
+  const cartItems = JSON.parse(localStorage.getItem("cartItems") || "[]");
+  cartCount.textContent = cartItems.length;
+}
+
+updateCartCount();
+
+const addToCart = (productName) => {
+  const cartItems = JSON.parse(localStorage.getItem('cartItems') || '[]');
+  cartItems.push(productName);
+  localStorage.setItem('cartItems', JSON.stringify(cartItems));
+  updateCartCount();
+}
+
+productList.addEventListener('click', ({target}) => {
+  if(target.closest('.product_btn-add-cart')) {
+    const productCard = target.closest('.store__product');
+    const productName = productCard.querySelector('.product__title').textContent;
+    addToCart(productName);
+  }
+})
